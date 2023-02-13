b3092ebce1e2831a6ca450534c83cb742e41ef2f
Andres David Velez Suaza <avelez0824@gmail.com>
2023-02-12 20:33:18 -0500

    add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import Logo from "../../utils/Logo";
+import "./style.scss";
+import facebook from "../../assets/icon-facebook.svg";
+import twitter from "../../assets/icon-twitter.svg";
+import pinterest from "../../assets/icon-pinterest.svg";
+import instagram from "../../assets/icon-instagram.svg";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer class="footer">
+      <div class="footer__top">
+        <Logo />
+        <div class="footer__top-right">
+          <img src={facebook} alt="" />
+          <img src={twitter} alt="" />
+          <img src={pinterest} alt="" />
+          <img src={instagram} alt="" />
+        </div>
+      </div>
+      <div class="footer__bottom">
+        <nav class="nav__links">
+          <a href="#about">About</a>
+          <a href="#careers">Careers</a>
+          <a href="#events">Events</a>
+          <a href="#products">Products</a>
+          <a href="#support">Support</a>
+        </nav>
+        <p class="footer__bottom-right">
+          &copy; 2021 Loopstudios. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
