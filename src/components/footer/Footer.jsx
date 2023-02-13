a86b6aa5a5981cdcbda6366d34633373705cb6a6
Andres David Velez Suaza <avelez0824@gmail.com>
2023-02-13 08:32:01 -0500

    add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,25 +8,25 @@ import instagram from "../../assets/icon-instagram.svg";
 
 function Footer() {
   return (
-    <footer class="footer">
-      <div class="footer__top">
+    <footer className="footer">
+      <div className="footer__left">
         <Logo />
-        <div class="footer__top-right">
-          <img src={facebook} alt="" />
-          <img src={twitter} alt="" />
-          <img src={pinterest} alt="" />
-          <img src={instagram} alt="" />
-        </div>
-      </div>
-      <div class="footer__bottom">
-        <nav class="nav__links">
+        <nav className="footer__left-links">
           <a href="#about">About</a>
           <a href="#careers">Careers</a>
           <a href="#events">Events</a>
           <a href="#products">Products</a>
           <a href="#support">Support</a>
         </nav>
-        <p class="footer__bottom-right">
+      </div>
+      <div className="footer__right">
+        <div className="footer__right-medias">
+          <img src={facebook} alt="" />
+          <img src={twitter} alt="" />
+          <img src={pinterest} alt="" />
+          <img src={instagram} alt="" />
+        </div>
+        <p className="footer__bottom-right">
           &copy; 2021 Loopstudios. All rights reserved.
         </p>
       </div>
